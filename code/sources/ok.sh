9bd703510d5cf3ed4301a201b026d43c6af8f46f
⠀⠀⠀⠀ <pxne031112hmcrgda1@bggem.mx>
2023-12-19 06:51:46 -0600

    update

---
+++ code/sources/ok.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+dir="patoBot";downScript="${dir}/downScript";downShell="${dir}/downShell";dataU="${dir}/dataUser"
+rekuest="https://raw.githubusercontent.com/drowkid-1/botsTG/main/code/rbot/source"
+[[ ! -d ${dir} ]] && {
+mkdir -p ${dir}/{downScript,downShell,dataUser,extras/} &> /dev/null
+lista="BotGen.sh botScript.sh ShellBot.sh"
+	for arqx in $lista; do
+	wget -O ${dir}/$arqx ${rekuest}/$arqx &> /dev/null
+	chmod +x ${dir}/$arqx
+	done
+	wget -O ${dir}/colores https://raw.githubusercontent.com/rudi9999/Herramientas/main/module/module &> /dev/null
+	source ${dir}/colores
+clear;msg -bar;msg -ne "token:";read token;msg -ne "id:";read idd
+jq --arg a "${idd}" --arg b "${token}" '{token: $b, data:{ admin:{ id: $a, username: "drowkid01" } } }' -n
+msg -bar;print_center -nverd "[✓]datos guardados[✓]"
+}
+
